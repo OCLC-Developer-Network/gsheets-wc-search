e47cd68b50374186aea374d42ec4a11402fce619
Coombs,Karen <coombsk@oclc.org>
2020-03-06 09:13:39 -0600

    Updated bib class

--- worldcat-discovery/src/lib.js
+++ worldcat-discovery/src/lib.js
@@ -1,36 +1,36 @@
-class Bib {
+module.exports = class Bib {
 	constructor(result){
 		this.record = JSON.parse(result);
 	}
-	function getID(){
+	
+	getID(){
 		return this.record.identifier.oclcNumber	    
 	}
 	
-	function getOCLCNumber(){
+	getOCLCNumber(){
 		return this.record.identifier.oclcNumber
 	}
 	
-	function getTitle(){
+	getTitle(){
 		let title = this.title.mainTitles[0]
 		title = title.replace(/\s\/+$/, "")
 		return title;
 	}
 	
-	function getAuthor(){
+	getAuthor(){
 		let author = this.record.contributor.creators[0].secondName.text + ', ' + this.record.contributor.creators[0].firstName.text
 	
 		return author;
 	}
-	function getISBNs(){
+	
+	getISBNs(){
 		let isbns = this.record.identifier.isbns
 		return isbns.join()	
 			
 	}
 	
-	function getMergedOCLCNumbers(){
+	getMergedOCLCNumbers(){
 		let mergedOclcNumbers = this.record.identifier.mergedOclcNumbers
 		return mergedOclcNumbers.join()
 	}
 }
-
-module.exports = Bib
