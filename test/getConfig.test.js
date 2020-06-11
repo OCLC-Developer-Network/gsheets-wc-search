5cbb5ee87fbc98ba5b747027500f0f622a47d7cf
Coombs,Karen <coombsk@oclc.org>
2020-06-10 23:11:17 -0500

    Added test for getConfig

---
+++ test/getConfig.test.js
@@ -0,0 +1,25 @@
+const expect = require('chai').expect;
+const fs = require('fs');
+const gas = require('gas-local');
+const sinon = require('sinon');
+const mocks = require("./mocks/gas-mocks");
+
+
+const lib = gas.require('./src', mocks);
+
+describe('when PropertiesService stubbed', () => {
+	describe('Get configuration data', () => {
+		it('returns a valid configuration object', () => {
+			let mockProperties = new mocks.Properties()
+			mockProperties.setProperty('apiKey', 'key')
+			mockProperties.setProperty('secret', 'secret')
+			mockProperties.setProperty('institutionSymbol', 'OCPSB')
+		    sinon.stub(mocks.PropertiesService, 'getUserProperties').returns(mockProperties);	    
+		    let config = lib.getConfig();
+		    expect(config).to.be.an("object");
+			expect(config.apiKey).to.equal('key');
+			expect(config.apiSecret).to.equal('secret');
+			expect(config.institutionSymbol).to.equal('OCPSB');
+		});	
+	});
+});
